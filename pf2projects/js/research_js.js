b6725f50d834a508c1b01d726f7e31bbb70f8858
Vanessa Montiel <vmontielmg@gmail.com>
2020-05-28 17:12:07 -0700

    initial commit
    files and images for portfolio

---
+++ pf2projects/js/research_js.js
@@ -0,0 +1,18 @@
+var elements = document.getElementsByClassName("column");
+
+// Declare a loop variable
+var i;
+
+// List View
+function listView() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.width = "100%";
+  }
+}
+
+// Grid View
+function gridView() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.width = "50%";
+  }
+}
